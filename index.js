634bd200ff253ecdd16d8153968abf7f1ed55c8f
David Irwin <redirwin@gmail.com>
2022-11-21 19:36:14 -0700

    All functionality and styling working.

--- index.js
+++ index.js
@@ -3,11 +3,54 @@ import { menuArray } from "/data.js"
 const billArray = []
 
 document.addEventListener('click', (e) => {
-
     e.target.dataset.add && handleAddClick(e.target.dataset.add)
     e.target.dataset.remove && handleRemoveClick(e.target.dataset.remove)
-    
+    e.target.id === "complete-order-button" && renderModal()
+    e.target.id === "exit-modal-button" && closeModal()
+    e.target.id === "pay-button" && handlePaymentSubmission()
 })
+
+function handlePaymentSubmission() {
+    const form = document.getElementById('payment-form')
+    const paymentData = {
+        'firstName' : form.firstName.value,
+        'lastName' : form.lastName.value,
+        'ccn': form.ccn.value,
+        'cvv': form.cvv.value
+    };
+    checkValidPayment(paymentData)  
+}
+
+function checkValidPayment(paymentData) {
+    console.log(paymentData)
+    if (
+      paymentData.firstName &&
+      paymentData.lastName &&
+      paymentData.ccn &&
+      paymentData.cvv
+    ) {
+      document.getElementById("order-modal").innerHTML = `
+        <div id="submitting-message">
+            <p>Submitting Order</p>
+            <img src="/assets/loading.svg" alt="submitting order"> 
+        </div>
+    `;
+      setTimeout(() => {
+        renderOrderSubmitted(paymentData.firstName);
+      }, 3000);
+    } 
+}
+
+function renderOrderSubmitted(name) {
+    closeModal()
+    document.getElementById('bill').innerHTML = `
+        <p id="thanks">Thanks, ${name}! Your order is on its way!</p>
+    `    
+}
+
+function closeModal() {
+    document.getElementById('order-modal').classList.add('no-show')
+}
 
 function handleAddClick(itemNumber) {
     const targetItem = menuArray.filter(menuItem => {
@@ -56,10 +99,14 @@ function getBillHtml() {
     })
     let billHtml = lineItems + `
         <div class="bill-total">
-            <div>Total price:</div>
+            <h3>Total price:</h3>
             <div>$${billTotal}</div>
         </div>    `
-    return `<h2>Your Order</h2>` + billHtml
+    return `
+        <h2>Your Order</h2>
+        ${billHtml}
+        <button id="complete-order-button">Complete Order</button>
+    `
 }
 
 function renderMenu() {
@@ -72,6 +119,23 @@ function renderBill() {
     } else {
         document.getElementById('bill').innerHTML = ""
     }
+}
+
+function renderModal() {
+    const modal = document.getElementById('order-modal')
+    modal.classList.remove('no-show')
+    modal.innerHTML = `
+        <h3>Enter card details <span id="exit-modal-button"></span></h3>
+        <form id="payment-form" name="payment-form">
+            <div id="name-fields">
+                <input type="text" name="firstName" placeholder="Enter your first name" id="firstName"required>
+                <input type="text" name="lastName" placeholder="Enter your last name" id="lastName"required>
+            </div>
+            <input type="text" name="ccn" placeholder="Enter card number" id="ccn" required>
+            <input type="text" name="cvv" placeholder="Enter CVV" id="cvv" required>
+            <button for="payment-form" id="pay-button" type="submit">Pay</button>
+        </form>
+    `
 }
 
 renderMenu();
